76af36d7cb41aa1c8d0e22af55eeb75054a527b3
cachemeifyoucan0 <masonmilly@gmail.com>
2020-11-14 15:02:35 -0500

    trying modal thingy

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import UserPreferences from UserPreferences;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <Component>UserPreferences</Component>
+    //   <StatusBar style="auto" />
+    // </View>
+    UserPreferences
   );
 }
 
