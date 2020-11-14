6e9ea5d7e68dbc6bc7c4db483fd1b5a8b0af7b7b
cachemeifyoucan0 <masonmilly@gmail.com>
2020-11-14 18:47:41 -0500

    added preference to app default

--- App.js
+++ App.js
@@ -1,16 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import UserPreferences from UserPreferences;
+import UserPreferenceForm from './UserPreferenceForm';
 
 export default function App() {
   return (
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working on your app!</Text>
-    //   <Component>UserPreferences</Component>
-    //   <StatusBar style="auto" />
-    // </View>
-    UserPreferences
+    <UserPreferenceForm/>
   );
 }
 
